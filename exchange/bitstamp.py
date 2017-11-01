df0e60dc9ffb0957e0bf5ff70e3f15138d735c73
ape364 <ape364@gmail.com>
2017-11-01 17:17:17 +0500

    add bitstamp

---
+++ exchange/bitstamp.py
@@ -0,0 +1,30 @@
+from exchange.base import BaseApi, Pair
+from exchange.exceptions import BaseExchangeException
+
+
+class BitstampApiException(BaseExchangeException):
+    pass
+
+
+class BitstampApi(BaseApi):
+    name = 'bitstamp'
+    url = 'https://www.bitstamp.net/'
+
+    async def tradable_pairs(self) -> set:
+        result = await self.get('https://www.bitstamp.net/api/v2/trading-pairs-info/')
+        return set(
+            Pair(
+                i['name'].split('/')[0],
+                i['name'].split('/')[1],
+            ) for i in result
+        )
+
+    def _raise_if_error(self, response: dict):
+        if not isinstance(response, list):
+            raise BitstampApiException('bitstamp api changed')
+
+    def _substitute(self, currency) -> str:
+        return currency
+
+    def ticker_url(self, pair: Pair) -> str:
+        return 'https://www.bitstamp.net/market/tradeview/'
